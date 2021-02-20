ddb5c717a933d225bf505ce0b0d7e8f957471d06
rifandani <ipandani2505@gmail.com>
2021-02-20 15:14:00 +0700

    :hammer: CHANGE: react table using DataTable.js

--- components/TableDashboard.tsx
+++ components/TableDashboard.tsx
@@ -1,27 +1,99 @@
-// files
+import Head from 'next/head';
+
 import TableSensors from './TableSensors';
 
 export default function AdminDashboard() {
   return (
-    <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-      <div className="px-6 py-8 mx-auto">
-        {/* tables */}
-        <div className="flex items-center justify-between">
-          <h3 className="text-3xl font-medium text-gray-700">
-            Sensors Data Table
-          </h3>
+    <>
+      <Head>
+        {/* Jquery js */}
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.min.js"
+          integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+          crossOrigin="anonymous"
+        />
 
-          {/* <button
+        {/* Bootstrap js */}
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+          integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+          crossOrigin="anonymous"
+        />
+
+        {/* Datatable CSS */}
+        <link
+          href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+
+        {/* Datatable JS */}
+        <script
+          type="text/javascript"
+          charSet="utf8"
+          src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.colVis.min.js"
+        />
+      </Head>
+
+      <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50">
+        <div className="px-6 py-8 mx-auto">
+          {/* tables */}
+          <div className="flex items-center justify-between">
+            <h3 className="text-3xl font-medium text-gray-700">
+              Sensors Data Table
+            </h3>
+
+            {/* <button
             onClick={() => {}}
             className="px-4 py-2 text-white bg-green-500 border rounded-md hover:bg-green-600"
-          >
+            >
             Add New Data
           </button> */}
-        </div>
+          </div>
 
-        {/* table using gridjs */}
-        <TableSensors />
-      </div>
-    </main>
+          {/* table using gridjs */}
+          <TableSensors />
+        </div>
+      </main>
+    </>
   );
 }
--- components/TableSensors.tsx
+++ components/TableSensors.tsx
@@ -1,9 +1,52 @@
-import { Grid } from 'gridjs-react';
+// import { Grid } from 'gridjs-react';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 // files
 
 export default function TableSensors() {
   const { data, error } = useSWR('/sensors', { refreshInterval: 10000 });
+
+  useEffect(() => {
+    data &&
+      // @ts-ignore
+      $('#empTable').DataTable({
+        data: data.sensors,
+        lengthMenu: [
+          [10, 60, 100, 600, 1000, 6000],
+          [
+            '10 rows',
+            '60 rows',
+            '100 rows',
+            '600 rows',
+            '1000 rows',
+            '6000 rows',
+          ],
+        ],
+        // processing: true,
+        // serverSide: true,
+        // serverMethod: 'post',
+        columns: [
+          { data: 'id' },
+          { data: 'temp' },
+          { data: 'alt' },
+          { data: 'press' },
+          { data: 'heat' },
+          { data: 'humid' },
+          { data: 'lux' },
+          { data: 'flame' },
+          { data: 'time' },
+        ],
+        dom: 'Bfrtip', // default lfrtip , ada juga Bfrtip , <"wrapper"flipt>, <"top"i>rt<"bottom"flp><"clear">
+        buttons: [
+          'colvis',
+          'pageLength',
+          'excelHtml5',
+          'csvHtml5',
+          'pdfHtml5',
+          'print',
+        ],
+      });
+  }, [data]);
 
   return (
     <div className="flex flex-col mt-4">
@@ -13,7 +56,7 @@ export default function TableSensors() {
 
           {!data && !error && 'Loading...'}
 
-          {data && (
+          {/* {data && (
             <Grid
               data={data?.sensors}
               columns={[
@@ -57,6 +100,31 @@ export default function TableSensors() {
                 limit: 10,
               }}
             />
+          )} */}
+
+          {data && (
+            //  Table
+            <table
+              id="empTable"
+              className="dataTable table-hover table-striped"
+              style={{ width: '100%' }}
+            >
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Temperatur (&#8451;)</th>
+                  <th>Altitude (m)</th>
+                  <th>Tekanan (hPa)</th>
+                  <th>Heat Index (&#8451;)</th>
+                  <th>Kelembaban (%)</th>
+                  <th>
+                    Iluminasi (lumen/m<sup>2</sup>)
+                  </th>
+                  <th>InfraRed (1/0)</th>
+                  <th>Date-Time</th>
+                </tr>
+              </thead>
+            </table>
           )}
         </div>
       </div>
